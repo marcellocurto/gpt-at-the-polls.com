7693296dae6d67fc721944e767a757d3badda08b
marcellocurto <marcello@roark.at>
2025-04-05 21:50:15 +0200

    add header logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,12 @@
 import { Link } from "@/components/links";
+import { LogoHeader } from "@/components/logo";
 
 export function Header() {
 	return (
 		<header className="px-2">
-			<div className="flex gap-6 rounded-b-4xl bg-gptorange-300 pt-8 pb-6 text-xl shadow-md">
+			<div className="bg-gptorange-300 flex gap-6 rounded-b-4xl pt-8 pb-6 text-xl shadow-md">
 				<div className="mx-auto flex w-full max-w-7xl justify-between px-10">
-					<div>
-						<Link className="font-black italic" href="/">
-							GPT At The Polls
-						</Link>
-					</div>
+					<LogoHeader />
 					<nav className="flex gap-4">
 						<Link className="font-medium italic" href="/polls">
 							Polls
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,4 +1,22 @@
 import { CircleCheckBig } from "lucide-react";
+import Link from "next/link";
+
+export function LogoHeader() {
+	return (
+		<div>
+			<Link className="group" href="/">
+				<div className="flex items-center justify-center gap-1 rounded-full px-4 py-2">
+					<div>
+						<CircleCheckBig className="group-hover:text-gptorange-100 group-hover:rotate-2 size-6 text-zinc-950 transition-all group-hover:scale-105" />
+					</div>
+					<p className="text-lg leading-0 font-bold text-zinc-950 italic">
+						GPT At the Polls
+					</p>
+				</div>
+			</Link>
+		</div>
+	);
+}
 
 export function Logo() {
 	return (
