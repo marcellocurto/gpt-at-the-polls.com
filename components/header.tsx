10ebbd3aa2e8c42ac55af20255f394b7d889f6c5
marcellocurto <marcello@roark.at>
2025-04-23 20:53:57 +0200

    rounded header footer

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import { LogoHeader } from "@/components/logo";
 
 export function Header() {
 	return (
-		<header className="px-2">
-			<div className="bg-gptorange-300/80 max-w-7xl mx-auto flex w-full items-center justify-between gap-6 rounded-b-4xl px-10 pt-8 pb-6 text-xl shadow-lg">
+		<header className="px-2 pt-4">
+			<div className="bg-gptorange-300/80 max-w-7xl mx-auto flex w-full items-center justify-between gap-6 rounded-4xl px-10 pt-8 pb-6 text-xl shadow-md">
 				<LogoHeader />
 				<nav className="flex gap-4">
 					<Link className="font-medium italic" href="/polls">
