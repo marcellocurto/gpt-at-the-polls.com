db34f7b3edae44d280d8681ac97ebc07c85a203e
marcellocurto <marcello@roark.at>
2025-05-04 06:48:42 +0200

    add index

---
+++ app/coolnes-index/page.tsx
@@ -0,0 +1,30 @@
+import {
+	PageLayout,
+	H1,
+	H2,
+	P,
+	Section,
+	SubSection,
+} from "@/components/page";
+import { Metadata } from "next";
+
+const title = "Index";
+
+export const metadata: Metadata = {
+	title,
+};
+
+export default function Page() {
+	return (
+		<PageLayout>
+			<H1>{title}</H1>
+
+			<Section>
+				<H2>In Progess</H2>
+				<SubSection>
+					<P>More to come soon.</P>
+				</SubSection>
+			</Section>
+		</PageLayout>
+	);
+}
