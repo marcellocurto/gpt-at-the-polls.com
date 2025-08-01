b316c969ec79515569e787af1083c14ef25b1262
marcellocurto <marcello@roark.at>
2025-08-01 16:06:35 +0200

    rename types

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,46 +1,4 @@
-export type VotesCache = {
-	description: string | undefined;
-	yesPersons: string[] | undefined;
-	noPersons: string[] | undefined;
-	date: string | undefined;
-	billId: string | undefined;
-};
-
-export type CongressCache = {
-	name: string;
-	years: string;
-};
-
-export type BillsCache = {
-	id: string | undefined;
-	title: string | undefined;
-	summary: string | undefined;
-	stateLink: string | undefined;
-	congress: string | undefined;
-	sourcePdf: string | undefined;
-	date: string | undefined;
-	yesVotes: string[] | undefined;
-	noVotes: string[] | undefined;
-};
-
-export type ModelCache = {
-	name: string | undefined;
-	description: string | undefined;
-	slug: string | undefined;
-	queries: Queries[];
-	selection: string | undefined;
-	companyName: string | undefined;
-};
-
-export type Queries = {
-	created: string | undefined;
-	justification: string | undefined;
-	session_id: string | undefined;
-	bill: BillsCache | undefined;
-	model: string | undefined;
-	politicalIndex: number;
-	vote: string | undefined;
-};
+import { Bill, Congress, Model, People, Queries, Vote } from "@/lib/types";
 
 const API_URL = process.env.API_URL!;
 
@@ -56,35 +14,13 @@ async function fetchFromAPI<T>(endpoint: string): Promise<T> {
 	return response.json();
 }
 
-async function postToAPI<T>(
-	endpoint: string,
-	body: { ids: string[] }
-): Promise<T> {
-	const response = await fetch(`${API_URL}${endpoint}`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(body),
-		cache: "force-cache",
-	});
-
-	if (!response.ok) {
-		throw new Error(
-			`Failed to post to ${endpoint}: ${response.statusText}`
-		);
-	}
-
-	return response.json();
-}
-
-export async function getModels(): Promise<ModelCache[]> {
-	return fetchFromAPI<ModelCache[]>("/api/models");
+export async function getModels(): Promise<Model[]> {
+	return fetchFromAPI<Model[]>("/api/models");
 }
 
 export async function getModelBySlug(
 	slug: string
-): Promise<ModelCache | undefined> {
+): Promise<Model | undefined> {
 	try {
 		const models = await getModels();
 		return models.find((model) => model.slug === slug);
@@ -93,22 +29,22 @@ export async function getModelBySlug(
 	}
 }
 
-export async function getBills(): Promise<BillsCache[]> {
-	return fetchFromAPI<BillsCache[]>("/api/bills");
+export async function getBills(): Promise<Bill[]> {
+	return fetchFromAPI<Bill[]>("/api/bills");
 }
 
-export async function getQueries(): Promise<QueriesFields[]> {
-	return fetchFromAPI<QueriesFields[]>("/api/queries");
+export async function getQueries(): Promise<Queries[]> {
+	return fetchFromAPI<Queries[]>("/api/queries");
 }
 
-export async function getVotes(): Promise<VotesCache[]> {
-	return fetchFromAPI<VotesCache[]>("/api/votes");
+export async function getVotes(): Promise<Vote[]> {
+	return fetchFromAPI<Vote[]>("/api/votes");
 }
 
-export async function getPeople(): Promise<PeopleFields[]> {
-	return fetchFromAPI<PeopleFields[]>("/api/people");
+export async function getPeople(): Promise<People[]> {
+	return fetchFromAPI<People[]>("/api/people");
 }
 
-export async function getCongressCache(): Promise<CongressCache[]> {
-	return fetchFromAPI<CongressCache[]>("/api/congress");
+export async function getCongressCache(): Promise<Congress[]> {
+	return fetchFromAPI<Congress[]>("/api/congress");
 }
---
+++ lib/types.ts
@@ -0,0 +1,47 @@
+export type Model = {
+	name: string | undefined;
+	description: string | undefined;
+	slug: string | undefined;
+	queries: Queries[];
+	selection: string | undefined;
+	companyName: string | undefined;
+};
+
+export type Queries = {
+	created: string | undefined;
+	justification: string | undefined;
+	session_id: string | undefined;
+	bill: Bill | undefined;
+	model: string | undefined;
+	politicalIndex: number;
+	vote: string | undefined;
+};
+
+export type Bill = {
+	id: string | undefined;
+	title: string | undefined;
+	summary: string | undefined;
+	stateLink: string | undefined;
+	congress: string | undefined;
+	sourcePdf: string | undefined;
+	date: string | undefined;
+	yesVotes: string[] | undefined;
+	noVotes: string[] | undefined;
+};
+
+export type Vote = {
+	description: string | undefined;
+	yesPersons: string[] | undefined;
+	noPersons: string[] | undefined;
+	date: string | undefined;
+	billId: string | undefined;
+};
+
+export type Congress = {
+	name: string;
+	years: string;
+};
+
+export type People = {
+	name: string;
+};
